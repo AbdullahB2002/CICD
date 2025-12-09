4185130fa17711474ef4ff52b33b445c89c9fbde
Abdullah <abdullahcse6@gmail.com>
2025-12-09 15:30:35 +0530

    Updated BaseTest and Azure pipeline YAML

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -2,18 +2,31 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
-	public WebDriver driver;
+    public WebDriver driver;
 
     @BeforeMethod
     public void launchBrowser() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+
+        // 🔥 This is required for Azure DevOps pipeline
+        if (System.getenv("CI") != null) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+        }
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
     }
 
@@ -23,5 +36,4 @@ public class BaseTest {
             driver.quit();
         }
     }
-
 }
